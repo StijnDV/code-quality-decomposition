6667bbddfa4de6cf9dd78ccd159f8719efb9b7cb
Stijn de Vries <stijnvries@hotmail.nl>
2017-06-13 14:42:16 +0200

    removed unused method in LocalBaron Finder

--- redbaron_python_scoping.py
+++ redbaron_python_scoping.py
@@ -13,10 +13,3 @@ class LocalBaronFinder:
 
     def scope_filter(self):
         return lambda node: node.parent_find('def') == self.scope
-
-    def global_name_nodes(self):
-        global_names = set()
-        global_statements = self.find_all('global')
-        for global_statement in global_statements:
-            global_names.update(global_statement.value)
-        return global_names
